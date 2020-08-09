8678b9cdbf0f8e298d8e1bdf73da0e354c60249a
Thomas Büchi Sagredo <thombuchi@gmail.com>
2020-08-09 12:28:06 -0400

    Initial Commit

---
+++ BLE MIDI Perpheral App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BLE MIDI Perpheral App
+//
+//  Created by Thomas Büchi on 09-08-20.
+//  Copyright © 2020 Thomas Büchi Sagredo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
