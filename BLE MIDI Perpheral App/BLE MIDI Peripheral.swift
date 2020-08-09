3f8aa18b469b75e68330dfe65f24a863b87fb0ca
Thomas Büchi Sagredo <thombuchi@gmail.com>
2020-08-09 12:37:31 -0400

    First File

---
+++ BLE MIDI Perpheral App/BLE MIDI Peripheral.swift
@@ -0,0 +1,9 @@
+//
+//  BLE MIDI Peripheral.swift
+//  BLE MIDI Perpheral App
+//
+//  Created by Thomas Büchi on 09-08-20.
+//  Copyright © 2020 Thomas Büchi Sagredo. All rights reserved.
+//
+
+import Foundation
